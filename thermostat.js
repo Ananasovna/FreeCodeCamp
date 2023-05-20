d49b9cffee519ea79311f7c1c5ed626b9b32f745
Ananasovna <a.n.ushkova@yandex.ru>
2023-05-20 15:06:43 +0200

    fix: delete comments from thermostat

--- thermostat.js
+++ thermostat.js
@@ -11,9 +11,4 @@ class Thermostat {
     this._temperature = newTemp;
   }
 }
-// Only change code above this line
 
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
